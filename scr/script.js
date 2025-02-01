ec17a55eb0436844262eb9526f715b82db2a2fe3
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-01 13:50:55 +0200

    done

--- scr/script.js
+++ scr/script.js
@@ -1,4 +1,3 @@
-
 const galleryItems = document.querySelectorAll('.gallery--item');
 
 const detailedImage = document.querySelector('.detailedContainer--image');
@@ -6,15 +5,22 @@ const detailedTitle = document.querySelector('.detailedContainer--title');
 
 galleryItems.forEach(item => {
   item.addEventListener('click', function () {
-
     const newImage = item.getAttribute('data-image');
     const newTitle = item.getAttribute('data-title');
     
-
+    // Устанавливаем новое изображение и новый заголовок
     detailedImage.src = newImage;
     detailedTitle.textContent = newTitle;
 
-    detailedImage.classList.add('animate__animated', 'animate__fadeIn');
+    // Убираем класс анимации, чтобы она сработала снова
+    detailedImage.classList.remove('animate__fadeIn');
+    detailedTitle.classList.remove('animate__fadeInUp');
+
+    // Применяем анимацию снова
+    // Используем setTimeout, чтобы дать браузеру время удалить старую анимацию
+    setTimeout(() => {
+      detailedImage.classList.add('animate__fadeIn');
+      detailedTitle.classList.add('animate__fadeInUp');
+    }, 10); // Маленькая задержка перед добавлением классов
   });
-  
 });
