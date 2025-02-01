23b1e7dfa6267e68df8f7f16052755ef45cfb18b
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-01 13:52:14 +0200

    done

--- scr/script.js
+++ scr/script.js
@@ -8,19 +8,17 @@ galleryItems.forEach(item => {
     const newImage = item.getAttribute('data-image');
     const newTitle = item.getAttribute('data-title');
     
-    // Устанавливаем новое изображение и новый заголовок
+
     detailedImage.src = newImage;
     detailedTitle.textContent = newTitle;
 
-    // Убираем класс анимации, чтобы она сработала снова
+
     detailedImage.classList.remove('animate__fadeIn');
     detailedTitle.classList.remove('animate__fadeInUp');
 
-    // Применяем анимацию снова
-    // Используем setTimeout, чтобы дать браузеру время удалить старую анимацию
     setTimeout(() => {
       detailedImage.classList.add('animate__fadeIn');
       detailedTitle.classList.add('animate__fadeInUp');
-    }, 10); // Маленькая задержка перед добавлением классов
+    }, 10); 
   });
 });
