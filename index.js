e2e93888ccc4b033c3f50ad399277e60f1000889
Olegshkradiuk <Olegwills40@gmail.com>
2025-03-23 21:03:07 +0200

    a

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-a = 10; 
+let a = 10;
+function fun1() {
+    a = 40;
+    if ( a == 40){
+        let a = 10
+        setTimeout(() => console.log('a', a), 0, a)
+    }
+}
+a = 30;
 
-console.log('a', window.a)
+fun1();
