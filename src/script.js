0de95cf1b3cb0c451f14c5fd56bc1b0b334b65ba
Olegshkradiuk <Olegwills40@gmail.com>
2025-04-01 20:59:12 +0300

    movieapp

--- src/script.js
+++ src/script.js
@@ -1,75 +1,148 @@
 const detailedImage = document.querySelector(".detailedContainer--image");
 const detailedTitle = document.querySelector(".detailedContainer--title");
-const detailedOverview = document.querySelector(".detailedContainer--overview");
-const galleryContainer = document.querySelector(".gallery");
+const formElem = document.getElementById("query-form");
+const mainElem = document.querySelector(".main");
+const inputElements = document.querySelectorAll("#query-form [name]");
+const API_KEY = "1eb9c2c4f46b71a5b4e658c14148c7cd";
+let year;
+let page = 1;
+const LANGUAGE = "en-us";
+const image_prefix = "https://image.tmdb.org/t/p/w500";
 
-const apiKey = "8c79629e2f6857b0b7addc2a59ac30b6";
+let galleryImages;
+const galleryElem = document.getElementById("movies_gallery");
+async function drawGalleryItems() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=${LANGUAGE}&primary_release_year=${year}&page=${page}&sort_by=popularity.desc`
+  );
+  const data = await response.json();
+  const itemsData = getItemsData(data.results);
+  const items = getItems(itemsData);
+  galleryElem.innerHTML = items;
+  galleryImages = document.querySelectorAll(".gallery--item_image");
+  addListeners();
+}
 
-async function drawMovies() {
-  try {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_companies=420`
-    );
+function getItemsData(data) {
+  const itemsData = data.map((record) => ({
+    itemImage: getImage(record.poster_path),
+    detailedImage: getImage(record.backdrop_path),
+    title: record.title,
+    detailedTitle: record.overview,
+  }));
+  return itemsData;
+}
 
+function getItems(itemsData) {
+  const items = itemsData.map(getItem);
+  return items.join();
+}
 
-    const data = await response.json();
-    galleryContainer.innerHTML = displayMovies(data.results);
-    addImage();
-  } catch (error) {
-    console.error("Request Error:", error);
+function getItem({ itemImage, detailedImage, title, detailedTitle }) {
+  return `<li class="gallery--item">
+          <img
+            src="${itemImage}"
+            alt="${title + " image"}"
+            class="gallery--item_image"
+            data-detailed-image="${detailedImage}"
+            data-detailed-title="${detailedTitle}"
+          />
+          <span class="gallery--item_title">${title} </span>
+        </li>`;
+}
+
+function getImage(image_id) {
+  return `${image_prefix}${image_id}`;
+}
+
+function addListeners() {
+  for (let i = 0; i < galleryImages.length; i++) {
+    galleryImages[i].addEventListener("click", function () {
+      setDetails(galleryImages[i]);
+    });
   }
 }
 
-function displayMovies(movies) {
-  return movies
-    .map((movie) => {
-      const image = movie.poster_path
-        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        : "placeholder.jpg";
-      const detailedImage = movie.backdrop_path
-        ? `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
-        : image;
-      const overview = movie.overview || "No description.";
-
-      return `
-        <li class="gallery--item">
-          <img
-            src="${image}"
-            alt="${movie.title}"
-            class="gallery--item_image"
-            data-detailed-image="${detailedImage}"
-            data-detailed-title="${movie.title}"
-            data-detailed-overview="${overview}"
-          />
-          <span class="gallery--item_title">${movie.title}</span>
-        </li>
-      `;
-    })
-    .join("");
+function setDetails(galleryImage) {
+  let image = galleryImage.getAttribute("data-detailed-image");
+  detailedImage.src = "";
+  detailedImage.src = image;
+  detailedTitle.innerHTML =
+    galleryImage.getAttribute("data-detailed-title") +
+    '<span class="for_ellipsis">...</span>';
+  animate();
 }
 
-function addImage() {
-  document.querySelectorAll(".gallery--item_image").forEach((image) => {
-    image.addEventListener("click", function () {
-      setDetails(image);
-    });
-  });
-}
-
-function setDetails(image) {
+function animate() {
   detailedImage.classList.remove("animation-up");
   detailedTitle.classList.remove("animation-down");
-
-  setTimeout(() => {
-
-    detailedImage.src = image.getAttribute("data-detailed-image");
-    detailedTitle.innerHTML = image.getAttribute("data-detailed-overview");
-
+  setTimeout(function () {
     detailedImage.classList.add("animation-up");
     detailedTitle.classList.add("animation-down");
-
-  }, 10);
-
+  }, 0);
 }
 
-drawMovies();
+
+formElem.addEventListener("submit", async function (event) {
+  event.preventDefault();
+  const data = getFormData();
+  year = +data.year;
+  page = 1;
+  console.log(year);
+  await drawGalleryItems();
+  mainElem.classList.remove("hidden");
+  formElem.classList.add("hidden");
+});
+
+
+document
+  .querySelector(".fold-button")
+  .addEventListener("click", async function () {
+    console.log("Next button clicked!");
+
+    page++;
+
+    await drawGalleryItems();
+
+    detailedImage.src = "images/film.webp";
+    detailedTitle.innerHTML = `Page number ${page} from Movie History year ${year} <span class="for_ellipsis">.....</span>`;
+
+
+    detailedImage.classList.remove("animation-up");
+    detailedTitle.classList.remove("animation-down");
+
+    setTimeout(() => {
+      detailedImage.classList.add("animation-up");
+      detailedTitle.classList.add("animation-down");
+    }, 0);
+  });
+
+function getFormData() {
+  const inputElementsArr = Array.from(inputElements);
+  const dataObj = inputElementsArr.reduce(
+    (res, curElem) => ({ ...res, [curElem.name]: curElem.value }),
+    {}
+  );
+  return dataObj;
+}
+
+function moveToInputData() {
+  mainElem.classList.add("hidden");
+  formElem.classList.remove("hidden");
+
+  detailedImage.src = "images/film.webp";
+  detailedTitle.innerHTML =
+    "gallery of the movies from themoviedb API. You may see many movies with images and short description. Sorted by popularity in the descending order <span class='for_ellipsis'>.....</span>";
+  page = 1;
+}
+
+document.querySelector(".fold-button").addEventListener("click", async function () {
+  console.log("Next button clicked!");
+  page++;
+  await drawGalleryItems();
+
+  detailedImage.src = "./images/film.webp";
+  detailedTitle.innerHTML = `Page number ${page} from Movie History year ${year} <span class="for_ellipsis">.....</span>`;
+
+  animate();
+});
